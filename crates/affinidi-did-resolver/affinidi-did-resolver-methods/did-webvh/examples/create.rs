6487336c8997da6e50135b3655136adb1eb48174
Glenn Gore <glenn.g@affinidi.com>
2025-05-25 10:10:17 +0800

    Working DID Document create wizard
    working verification methods
    working service definitions

--- crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/create.rs
+++ crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/create.rs
@@ -3,11 +3,15 @@
 */
 
 use affinidi_secrets_resolver::secrets::Secret;
-use affinidi_tdk::dids::{DID, KeyType};
+use affinidi_tdk::{
+    dids::{DID, KeyType},
+    meeting_place::find_api_service_endpoint,
+};
 use anyhow::Result;
 use console::style;
-use dialoguer::{Confirm, Input, theme::ColorfulTheme};
+use dialoguer::{Confirm, Editor, Input, MultiSelect, Select, theme::ColorfulTheme};
 use did_webvh::url::WebVHURL;
+use serde_json::{Value, json};
 use tracing_subscriber::filter;
 use url::Url;
 
@@ -67,7 +71,9 @@ async fn main() -> Result<()> {
 
     show_banner();
 
+    // ************************************************************************
     // Step 1: Get the URLs for this DID
+    // ************************************************************************
     let (http_url, webvh_did) = loop {
         match get_address() {
             Ok((url, did)) => break (url, did),
@@ -86,7 +92,9 @@ async fn main() -> Result<()> {
     );
     println!();
 
+    // ************************************************************************
     // Step 2: Create authorization keys to manage this DID
+    // ************************************************************************
     let authorizing_keys = loop {
         match get_authorization_keys(&webvh_did) {
             Ok(keys) => break keys,
@@ -105,9 +113,42 @@ async fn main() -> Result<()> {
     );
     println!("{}", style("Authorizing Keys:").color256(69),);
     for k in &authorizing_keys {
-        println!("\t{}", style(&k.0).color256(141));
+        println!("\t{}", style(&k.get_public_keymultibase()?).color256(141));
     }
     println!();
+
+    // ************************************************************************
+    // Step 3: Create the DID Document
+    // ************************************************************************
+    let did_document = loop {
+        match create_did_document(&webvh_did) {
+            Ok(keys) => break keys,
+            Err(_) => {
+                println!(
+                    "{}",
+                    style("Invalid did document, please try again").color256(196)
+                );
+                continue;
+            }
+        }
+    };
+
+    println!();
+    println!(
+        "{} {}",
+        style("webvh DID:").color256(69),
+        style(&webvh_did).color256(141)
+    );
+    println!("{}", style("Authorizing Keys:").color256(69),);
+    for k in &authorizing_keys {
+        println!("\t{}", style(&k.get_public_keymultibase()?).color256(141));
+    }
+    println!(
+        "{}\n{}",
+        style("DID Document:").color256(69),
+        style(&serde_json::to_string_pretty(&did_document).unwrap()).color256(141)
+    );
+
     Ok(())
 }
 
@@ -233,7 +274,7 @@ fn get_address() -> Result<(String, String)> {
 }
 
 // Create authorization keys for the DID
-fn get_authorization_keys(webvh_did: &str) -> Result<Vec<(String, Secret)>> {
+fn get_authorization_keys(webvh_did: &str) -> Result<Vec<Secret>> {
     println!(
         "{} {} {}",
         style("A set of keys are required to manage").color256(69),
@@ -253,13 +294,13 @@ fn get_authorization_keys(webvh_did: &str) -> Result<Vec<(String, Secret)>> {
         style(")").color256(69)
     );
 
-    let mut keys: Vec<(String, Secret)> = Vec::new();
+    let mut keys: Vec<Secret> = Vec::new();
 
     loop {
         if !keys.is_empty() {
             println!("{}", style("Authorizing Keys:").color256(69),);
             for k in &keys {
-                println!("\t{}", style(&k.0).color256(141));
+                println!("\t{}", style(&k.get_public_keymultibase()?).color256(141));
             }
             if !Confirm::with_theme(&ColorfulTheme::default())
                 .with_prompt("Do you want to add another key?")
@@ -275,6 +316,29 @@ fn get_authorization_keys(webvh_did: &str) -> Result<Vec<(String, Secret)>> {
             .default(false)
             .interact()?
         {
+            let public: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("publicKeyMultibase")
+                .interact()
+                .unwrap();
+
+            let private: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("privateKeyMultibase")
+                .interact()
+                .unwrap();
+
+            if Confirm::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!(
+                    "Use public({}) and private({}) as an authorized key?",
+                    &public, &private
+                ))
+                .interact()
+                .unwrap()
+            {
+                keys.push(Secret::from_multibase(
+                    "", // No controller for this key
+                    &public, &private,
+                )?);
+            }
         } else {
             // Generate a new key
             let key = DID::generate_did_key(KeyType::Ed25519).unwrap();
@@ -290,9 +354,373 @@ fn get_authorization_keys(webvh_did: &str) -> Result<Vec<(String, Secret)>> {
                 style("privateKeyMultibase:").color256(69),
                 style(&key.1.get_private_keymultibase()?).color256(214)
             );
-            keys.push((key.0, key.1));
+            keys.push(key.1);
         }
     }
 
     Ok(keys)
+}
+
+// Create DID Document
+fn create_did_document(webvh_did: &str) -> Result<Value> {
+    println!(
+        "{} {}",
+        style("Create a DID Document for:").color256(69),
+        style(webvh_did).color256(141),
+    );
+
+    let mut did_document = json!({
+      "id": webvh_did,
+      "@context": [
+        "https://www.w3.org/ns/did/v1",
+        "https://www.w3.org/ns/cid/v1",
+      ],
+      "verificationMethod": [],
+      "authentication": [],
+      "assertionMethod": [],
+      "keyAgreement": [],
+      "capabilityInvocation": [],
+      "capabilityDelegation": [],
+    });
+
+    // Controller
+    if let Some(controller) = controller() {
+        did_document["controller"] = json!(controller);
+    }
+
+    // AlsoKnownAs
+    let also_known_as = also_known_as();
+    if !also_known_as.is_empty() {
+        did_document["alsoKnownAs"] = json!(also_known_as);
+    }
+
+    // Add Verification Methods
+    get_verification_methods(webvh_did, &mut did_document);
+
+    println!();
+    println!(
+        "{}\n{}",
+        style("DID Document").color256(69),
+        style(serde_json::to_string_pretty(&did_document).unwrap()).color256(34)
+    );
+    println!();
+
+    // Add Services
+    add_services(webvh_did, &mut did_document);
+
+    println!();
+    println!(
+        "{}\n{}",
+        style("DID Document").color256(69),
+        style(serde_json::to_string_pretty(&did_document).unwrap()).color256(34)
+    );
+    println!();
+
+    let did_document = if Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Would you like to edit this DID Document?")
+        .default(false)
+        .interact()
+        .unwrap()
+    {
+        if let Some(document) = Editor::new()
+            .extension("json")
+            .edit(&serde_json::to_string_pretty(&did_document).unwrap())
+            .unwrap()
+        {
+            match serde_json::from_str(&document) {
+                Ok(document) => document,
+                Err(e) => {
+                    println!("{}", style("Invalid DID Document!").color256(196));
+                    println!("\t{}", style(e.to_string()).color256(196));
+                    did_document
+                }
+            }
+        } else {
+            did_document
+        }
+    } else {
+        did_document
+    };
+
+    Ok(did_document)
+}
+
+// Handle if the did needs a controller
+fn controller() -> Option<String> {
+    loop {
+        if Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("Does this DID have a controller?")
+            .default(false)
+            .interact()
+            .unwrap()
+        {
+            let input = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Controller")
+                .interact()
+                .unwrap();
+            if Confirm::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("Use ({}) as controller?", &input))
+                .interact()
+                .unwrap()
+            {
+                break Some(input);
+            }
+        } else {
+            break None;
+        }
+    }
+}
+
+// Is this controller also known as another DID?
+fn also_known_as() -> Vec<String> {
+    let mut others: Vec<String> = Vec::new();
+    if Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Is this DID also known as another DID?")
+        .default(false)
+        .interact()
+        .unwrap()
+    {
+        loop {
+            let input = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Other DID")
+                .interact()
+                .unwrap();
+            if Confirm::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("Use ({}) as alias?", &input))
+                .interact()
+                .unwrap()
+            {
+                others.push(input);
+            }
+            if !Confirm::with_theme(&ColorfulTheme::default())
+                .with_prompt("Add another alias?")
+                .default(false)
+                .interact()
+                .unwrap()
+            {
+                break;
+            }
+        }
+    }
+    others
+}
+
+// Create Verification Methods
+fn get_verification_methods(webvh_did: &str, doc: &mut Value) {
+    let mut key_id: u32 = 0;
+    let mut success_count: u32 = 0;
+
+    loop {
+        let vm_id: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Verification ID")
+            .default(format!("{}#key-{}", webvh_did, key_id))
+            .interact()
+            .unwrap();
+
+        let secret = create_key(&vm_id);
+        let vm = json!({
+            "id": vm_id.clone(),
+            "type": "Multikey",
+            "publicKeyMultibase": secret.get_public_keymultibase().unwrap(),
+            "controller": webvh_did.to_string()
+        });
+
+        let relationships = [
+            "authentication",
+            "assertionMethod",
+            "keyAgreement",
+            "capabilityInvocation",
+            "capabilityDelegation",
+        ];
+        let purpose = MultiSelect::with_theme(&ColorfulTheme::default())
+            .with_prompt("What are the relationships of this Verification Method?")
+            .items(&relationships)
+            .defaults(&[true, true, true, false, false]) // Default to authentication
+            .interact()
+            .unwrap();
+
+        println!(
+            "{}\n{}",
+            style("Verification Method:").color256(69),
+            style(serde_json::to_string_pretty(&vm).unwrap()).color256(141)
+        );
+        print!("{} ", style("Relationships:").color256(69),);
+        for r in &purpose {
+            print!("{} ", style(relationships[r.to_owned()]).color256(141));
+        }
+        println!();
+
+        if Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("Accept this Verification Method?")
+            .default(true)
+            .interact()
+            .unwrap()
+        {
+            success_count += 1;
+            key_id += 1;
+
+            // Add to document
+            doc["verificationMethod"]
+                .as_array_mut()
+                .unwrap()
+                .push(vm.clone());
+            for r in purpose {
+                match r {
+                    0 => doc["authentication"]
+                        .as_array_mut()
+                        .unwrap()
+                        .push(Value::String(vm_id.clone())),
+                    1 => doc["assertionMethod"]
+                        .as_array_mut()
+                        .unwrap()
+                        .push(Value::String(vm_id.clone())),
+                    2 => doc["keyAgreement"]
+                        .as_array_mut()
+                        .unwrap()
+                        .push(Value::String(vm_id.clone())),
+                    3 => doc["capabilityInvocation"]
+                        .as_array_mut()
+                        .unwrap()
+                        .push(Value::String(vm_id.clone())),
+                    4 => doc["capabilityDelegation"]
+                        .as_array_mut()
+                        .unwrap()
+                        .push(Value::String(vm_id.clone())),
+                    _ => {}
+                }
+            }
+        }
+        if success_count > 0
+            && !Confirm::with_theme(&ColorfulTheme::default())
+                .with_prompt("Add another Verification Method?")
+                .default(false)
+                .interact()
+                .unwrap()
+        {
+            break;
+        }
+    }
+}
+
+fn create_key(id: &str) -> Secret {
+    let items = vec![
+        KeyType::Ed25519.to_string(),
+        KeyType::P256.to_string(),
+        KeyType::Secp256k1.to_string(),
+        KeyType::P384.to_string(),
+    ];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("What key type?")
+        .items(&items)
+        .default(0) // Default to Ed25519
+        .interact()
+        .unwrap();
+
+    let (_, mut secret) =
+        DID::generate_did_key(KeyType::try_from(items[selection].as_str()).unwrap()).unwrap();
+
+    secret.id = id.to_string();
+    secret
+}
+
+// Add Services
+fn add_services(webvh_did: &str, doc: &mut Value) {
+    doc["service"] = json!([]);
+    let service_choice = ["Simple", "Complex"];
+    let mut service_id: u32 = 0;
+
+    let default_service_map = r#"{
+  "id": "REPLACE",
+  "type": "DIDCommMessaging",
+  "serviceEndpoint": [
+    {
+      "accept": [
+        "didcomm/v2"
+      ],
+      "routingKeys": [],
+      "uri": "http://localhost:8000/api"
+    }
+  ]
+}"#;
+    loop {
+        if !Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("Add a service for this DID?")
+            .default(false)
+            .interact()
+            .unwrap()
+        {
+            return;
+        }
+
+        let service = match Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Service type?")
+            .items(&service_choice)
+            .default(0) // Default to Ed25519
+            .interact()
+            .unwrap()
+        {
+            0 => {
+                // Simple
+                let service_id: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Service ID")
+                    .default(format!("{}#service-{}", webvh_did, service_id))
+                    .interact()
+                    .unwrap();
+
+                let service_type: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Service Type")
+                    .interact()
+                    .unwrap();
+                let service_endpoint: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Service Endpoint")
+                    .interact()
+                    .unwrap();
+
+                let service = json!({
+                    "id": service_id,
+                    "type": service_type,
+                    "serviceEndpoint": service_endpoint
+                });
+                service
+            }
+            1 => {
+                // Complex
+                let template = default_service_map
+                    .replace("REPLACE", &format!("{}#service-{}", webvh_did, service_id));
+                if let Some(service) = Editor::new().extension("json").edit(&template).unwrap() {
+                    match serde_json::from_str(&service) {
+                        Ok(service) => service,
+                        Err(e) => {
+                            println!("{}", style("Invalid service definition").color256(196));
+                            println!("\t{}", style(e.to_string()).color256(196));
+                            continue;
+                        }
+                    }
+                } else {
+                    println!("Service definition wasn't saved!");
+                    continue;
+                }
+            }
+            _ => continue,
+        };
+
+        println!();
+        println!(
+            "{}\n{}",
+            style("Service:").color256(69),
+            style(serde_json::to_string_pretty(&service).unwrap()).color256(141)
+        );
+        println!();
+
+        if Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("Accept this Service?")
+            .default(true)
+            .interact()
+            .unwrap()
+        {
+            doc["service"].as_array_mut().unwrap().push(service);
+            service_id += 1;
+        }
+    }
 }
