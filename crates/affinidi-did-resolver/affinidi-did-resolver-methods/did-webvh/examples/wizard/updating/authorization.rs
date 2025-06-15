5cfec2a28a1ffcb36a1d4de0d3b58a559e565337
Glenn Gore <glenn.g@affinidi.com>
2025-06-15 16:17:02 +0800

    Cleaning up structure of wizard

--- crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/wizard/updating/authorization.rs
+++ crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/wizard/updating/authorization.rs
@@ -1,154 +1,18 @@
 /*!
-*   Tasks relating to editing an existing webvh DID go here
+*   Modifying authorization keys depends on whether the DID is
+*   in pre-rotation mode or not
 */
+
+use crate::{ConfigInfo, create_next_key_hashes, get_keys};
 use affinidi_secrets_resolver::secrets::Secret;
 use anyhow::{Result, bail};
-use console::style;
-use dialoguer::{Confirm, Input, MultiSelect, Select, theme::ColorfulTheme};
+use dialoguer::{Confirm, MultiSelect, theme::ColorfulTheme};
 use did_webvh::{DIDWebVHError, log_entry::LogEntry, parameters::Parameters};
-
-use crate::{ConfigInfo, create_next_key_hashes, edit_did_document, get_keys};
-
-pub async fn edit_did() -> Result<()> {
-    let file_path: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("DID LogEntry File?")
-        .default("did.jsonl".to_string())
-        .interact()
-        .unwrap();
-
-    let (log_entry, meta_data) = LogEntry::get_log_entry_from_file(&file_path, None, None, None)?;
-
-    // Load the secrets
-    let mut config_info = ConfigInfo::read_from_file("secrets.json")
-        .map_err(|e| DIDWebVHError::ParametersError(format!("Failed to read secrets: {}", e)))?;
-
-    println!(
-        "{}\n{}",
-        style("Log Entry Parameters:").color256(69),
-        style(serde_json::to_string_pretty(&log_entry.parameters).unwrap()).color256(34),
-    );
-    println!();
-    println!(
-        "{}\n{}\n\n{}",
-        style("Log Entry Metadata:").color256(69),
-        style(serde_json::to_string_pretty(&meta_data).unwrap()).color256(34),
-        style("Successfully Loaded").color256(34).blink(),
-    );
-
-    println!();
-
-    let menu = vec![
-        "Modify DID",
-        "Move to a new domain (portability)?",
-        "Revoke this DID?",
-        "Back",
-    ];
-
-    loop {
-        let selection = Select::with_theme(&ColorfulTheme::default())
-            .with_prompt("Editing DID")
-            .items(&menu)
-            .default(0)
-            .interact()
-            .unwrap();
-
-        match selection {
-            0 => {
-                println!(
-                    "{}",
-                    style("Modifying DID Document and/or Parameters").color256(69)
-                );
-                let new_state = if Confirm::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Edit the DID Document?")
-                    .default(false)
-                    .interact()?
-                {
-                    edit_did_document(&log_entry.state)?
-                } else {
-                    log_entry.state.clone()
-                };
-                let new_params = update_parameters(&log_entry, &mut config_info)?;
-                let diff_params = log_entry.parameters.diff(&new_params)?;
-                println!("{}", serde_json::to_string_pretty(&diff_params).unwrap());
-            }
-            1 => {
-                println!("{}", style("Migrate to a new domain").color256(69));
-            }
-            2 => {
-                println!("{}", style("Revoke this DID").color256(69));
-            }
-            3 => {
-                break;
-            }
-            _ => {
-                println!("{}", style("Invalid selection...").color256(196));
-                continue;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-/// Run UI for creating new parameter set
-/// Returns: New Parameters
-fn update_parameters(old_log_entry: &LogEntry, secrets: &mut ConfigInfo) -> Result<Parameters> {
-    let mut new_params = Parameters::default();
-
-    // updateKeys and nextKeyHashes
-    update_authorization_keys(old_log_entry, &mut new_params, secrets)?;
-    println!(
-        "{}{}{}",
-        style("Pre-rotation (").color256(69),
-        if new_params.pre_rotation_active {
-            style("enabled").color256(34)
-        } else {
-            style("disabled").color256(214)
-        },
-        style(")").color256(69)
-    );
-    println!(
-        "{}\n{}",
-        style("nextKeyHashes: ").color256(69),
-        style(serde_json::to_string_pretty(&new_params.next_key_hashes).unwrap()).color256(34)
-    );
-    println!(
-        "{}\n{}",
-        style("updateKeys: ").color256(69),
-        style(serde_json::to_string_pretty(&new_params.update_keys).unwrap()).color256(34)
-    );
-
-    // Turn portability off
-    if let Some(portable) = old_log_entry.parameters.portable {
-        if portable {
-            // Portable
-            if Confirm::with_theme(&ColorfulTheme::default())
-                .with_prompt("Disable portability for this DID?")
-                .default(false)
-                .interact()
-                .map_err(|e| {
-                    DIDWebVHError::ParametersError(format!(
-                        "Invalid selection on portability: {}",
-                        e
-                    ))
-                })?
-            {
-                // Disable portability
-                new_params.portable = Some(false);
-            } else {
-                // Keep portability
-                new_params.portable = Some(true);
-            }
-        }
-    }
-
-    Ok(new_params)
-}
 
 /// Handles all possible states of updating updateKeys including pre-rotation and non-pre-rotation
 /// modes. updateKeys and NextKeyHashes are modified here
 /// Returns authorization key for this update
-fn update_authorization_keys(
+pub fn update_authorization_keys(
     old_log_entry: &LogEntry,
     new_params: &mut Parameters,
     existing_secrets: &mut ConfigInfo,
---
+++ crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/wizard/updating/mod.rs
@@ -0,0 +1,171 @@
+/*!
+*   Tasks relating to editing an existing webvh DID go here
+*/
+use crate::{ConfigInfo, edit_did_document, updating::authorization::update_authorization_keys};
+use anyhow::Result;
+use console::style;
+use dialoguer::{Confirm, Input, Select, theme::ColorfulTheme};
+use did_webvh::{DIDWebVHError, log_entry::LogEntry, parameters::Parameters};
+
+mod authorization;
+mod witness;
+
+pub async fn edit_did() -> Result<()> {
+    let file_path: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("DID LogEntry File?")
+        .default("did.jsonl".to_string())
+        .interact()
+        .unwrap();
+
+    let (log_entry, meta_data) = LogEntry::get_log_entry_from_file(&file_path, None, None, None)?;
+
+    // Load the secrets
+    let mut config_info = ConfigInfo::read_from_file("secrets.json")
+        .map_err(|e| DIDWebVHError::ParametersError(format!("Failed to read secrets: {}", e)))?;
+
+    println!(
+        "{}\n{}",
+        style("Log Entry Parameters:").color256(69),
+        style(serde_json::to_string_pretty(&log_entry.parameters).unwrap()).color256(34),
+    );
+    println!();
+    println!(
+        "{}\n{}\n\n{}",
+        style("Log Entry Metadata:").color256(69),
+        style(serde_json::to_string_pretty(&meta_data).unwrap()).color256(34),
+        style("Successfully Loaded").color256(34).blink(),
+    );
+
+    println!();
+
+    let menu = vec![
+        "Modify DID",
+        "Move to a new domain (portability)?",
+        "Revoke this DID?",
+        "Back",
+    ];
+
+    loop {
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Editing DID")
+            .items(&menu)
+            .default(0)
+            .interact()
+            .unwrap();
+
+        match selection {
+            0 => {
+                println!(
+                    "{}",
+                    style("Modifying DID Document and/or Parameters").color256(69)
+                );
+
+                // ************************************************************************
+                // Change the DID Document?
+                // ************************************************************************
+                let new_state = if Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Edit the DID Document?")
+                    .default(false)
+                    .interact()?
+                {
+                    edit_did_document(&log_entry.state)?
+                } else {
+                    log_entry.state.clone()
+                };
+
+                // ************************************************************************
+                // Change webvh Parameters
+                // ************************************************************************
+                let new_params = update_parameters(&log_entry, &mut config_info)?;
+                let diff_params = log_entry.parameters.diff(&new_params)?;
+                println!("{}", serde_json::to_string_pretty(&diff_params).unwrap());
+            }
+            1 => {
+                println!("{}", style("Migrate to a new domain").color256(69));
+            }
+            2 => {
+                println!("{}", style("Revoke this DID").color256(69));
+            }
+            3 => {
+                break;
+            }
+            _ => {
+                println!("{}", style("Invalid selection...").color256(196));
+                continue;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Run UI for creating new parameter set
+/// Returns: New Parameters
+fn update_parameters(old_log_entry: &LogEntry, secrets: &mut ConfigInfo) -> Result<Parameters> {
+    let mut new_params = Parameters::default();
+
+    // ************************************************************************
+    // Portability
+    // ************************************************************************
+    update_authorization_keys(old_log_entry, &mut new_params, secrets)?;
+    println!(
+        "{}{}{}",
+        style("Pre-rotation (").color256(69),
+        if new_params.pre_rotation_active {
+            style("enabled").color256(34)
+        } else {
+            style("disabled").color256(214)
+        },
+        style(")").color256(69)
+    );
+    println!(
+        "{}\n{}",
+        style("nextKeyHashes: ").color256(69),
+        style(serde_json::to_string_pretty(&new_params.next_key_hashes).unwrap()).color256(34)
+    );
+    println!(
+        "{}\n{}",
+        style("updateKeys: ").color256(69),
+        style(serde_json::to_string_pretty(&new_params.update_keys).unwrap()).color256(34)
+    );
+
+    // ************************************************************************
+    // Portability
+    // ************************************************************************
+    if let Some(portable) = old_log_entry.parameters.portable {
+        if portable {
+            // Portable
+            if Confirm::with_theme(&ColorfulTheme::default())
+                .with_prompt("Disable portability for this DID?")
+                .default(false)
+                .interact()
+                .map_err(|e| {
+                    DIDWebVHError::ParametersError(format!(
+                        "Invalid selection on portability: {}",
+                        e
+                    ))
+                })?
+            {
+                // Disable portability
+                new_params.portable = Some(false);
+            } else {
+                // Keep portability
+                new_params.portable = Some(true);
+            }
+        }
+    }
+
+    // ************************************************************************
+    // Witnesses
+    // ************************************************************************
+
+    // ************************************************************************
+    // Watchers
+    // ************************************************************************
+
+    // ************************************************************************
+    // TTL
+    // ************************************************************************
+
+    Ok(new_params)
+}
