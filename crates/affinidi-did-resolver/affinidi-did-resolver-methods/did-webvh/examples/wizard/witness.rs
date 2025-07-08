89a66d9e79c451000aa2b8a7ce999f253a8d72cd
Glenn Gore <glenn.g@affinidi.com>
2025-07-08 08:39:37 +0800

    Final Commit

--- crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/wizard/witness.rs
+++ crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/examples/wizard/witness.rs
@@ -1,5 +1,5 @@
 use crate::ConfigInfo;
-use affinidi_data_integrity::{DataIntegrityProof, SigningDocument};
+use affinidi_data_integrity::DataIntegrityProof;
 use anyhow::{Result, bail};
 use console::style;
 use did_webvh::{
@@ -31,7 +31,6 @@ pub fn witness_log_entry(
         style(") proofs from witnesses").color256(69)
     );
 
-    let mut doc_to_sign: SigningDocument = log_entry.try_into()?;
     for witness in &witnesses.witnesses {
         // Get secret for Witness
         let Some(secret) = secrets.witnesses.get(&witness.id) else {
@@ -39,34 +38,27 @@ pub fn witness_log_entry(
         };
 
         // Generate Signature
-        DataIntegrityProof::sign_jcs_data(&mut doc_to_sign, secret).map_err(|e| {
-            DIDWebVHError::SCIDError(format!(
-                "Couldn't generate Data Integrity Proof for LogEntry. Reason: {}",
-                e
-            ))
-        })?;
+        let proof =
+            DataIntegrityProof::sign_jcs_data(&log_entry, None, secret, None).map_err(|e| {
+                DIDWebVHError::SCIDError(format!(
+                    "Couldn't generate Data Integrity Proof for LogEntry. Reason: {}",
+                    e
+                ))
+            })?;
 
         // Save proof to collection
-        if let Some(proof) = &doc_to_sign.proof {
-            witness_proofs
-                .add_proof(&log_entry.version_id, proof, false)
-                .map_err(|e| {
-                    DIDWebVHError::WitnessProofError(format!("Error adding proof: {}", e))
-                })?;
+        witness_proofs
+            .add_proof(&log_entry.version_id, &proof, false)
+            .map_err(|e| DIDWebVHError::WitnessProofError(format!("Error adding proof: {}", e)))?;
 
-            doc_to_sign.proof = None; // Reset proof for next witness
-
-            println!(
-                "{}{}{}{}{}",
-                style("Witness (").color256(69),
-                style(&witness.id).color256(45),
-                style("): Successfully witnessed LogEntry (").color256(69),
-                style(&log_entry.version_id).color256(45),
-                style(")").color256(69),
-            );
-        } else {
-            bail!("No proof generated from witness ({})!", witness.id);
-        }
+        println!(
+            "{}{}{}{}{}",
+            style("Witness (").color256(69),
+            style(&witness.id).color256(45),
+            style("): Successfully witnessed LogEntry (").color256(69),
+            style(&log_entry.version_id).color256(45),
+            style(")").color256(69),
+        );
     }
     // Strip out any duplicate records where we can
     witness_proofs.write_optimise_records()?;
--- crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/tests/first_log_entry.rs
+++ crates/affinidi-did-resolver/affinidi-did-resolver-methods/did-webvh/tests/first_log_entry.rs
@@ -1,4 +1,4 @@
-use affinidi_data_integrity::{SignedDocument, verification_proof::verify_data};
+use affinidi_data_integrity::{DataIntegrityProof, verification_proof::verify_data};
 use common::load_test_file;
 use did_webvh::log_entry::LogEntry;
 
@@ -30,10 +30,23 @@ fn test_first_log_entry_deactivated_error() {
 fn test_first_log_entry_verify_signature() {
     let first_log_entry = load_test_file("tests/test_vectors/first_log_entry_verify_full.jsonl");
 
-    let first_log_entry: SignedDocument =
-        serde_json::from_str(&first_log_entry).expect("Failed to parse first log entry JSON");
+    let mut first_log_entry =
+        serde_json::to_value(&first_log_entry).expect("Failed to parse first log entry JSON");
 
-    assert!(verify_data(&first_log_entry).is_ok());
+    let proof: DataIntegrityProof = serde_json::from_value(
+        first_log_entry
+            .get("proof")
+            .expect("Failed to get proof from first log entry")
+            .clone(),
+    )
+    .expect("Couldn't extra proof");
+
+    first_log_entry
+        .as_object_mut()
+        .expect("Failed to get object from first log entry")
+        .remove("proof");
+
+    assert!(verify_data(&first_log_entry, None, &proof).is_ok());
 }
 
 #[test]
@@ -41,10 +54,23 @@ fn test_first_log_entry_verify_signature_tampered() {
     let first_log_entry =
         load_test_file("tests/test_vectors/first_log_entry_verify_tampered.jsonl");
 
-    let first_log_entry: SignedDocument =
-        serde_json::from_str(&first_log_entry).expect("Failed to parse first log entry JSON");
+    let mut first_log_entry =
+        serde_json::to_value(&first_log_entry).expect("Failed to parse first log entry JSON");
 
-    assert!(verify_data(&first_log_entry).is_err());
+    let proof: DataIntegrityProof = serde_json::from_value(
+        first_log_entry
+            .get("proof")
+            .expect("Failed to get proof from first log entry")
+            .clone(),
+    )
+    .expect("Couldn't extra proof");
+
+    first_log_entry
+        .as_object_mut()
+        .expect("Failed to get object from first log entry")
+        .remove("proof");
+
+    assert!(verify_data(&first_log_entry, None, &proof).is_err());
 }
 
 #[test]
