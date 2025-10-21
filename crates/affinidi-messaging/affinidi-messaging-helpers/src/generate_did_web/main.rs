0ef60fd49c9815d7f08644c717e866f1c934e7e7
Francis Pineda <francis.p@affinidi.com>
2025-10-21 15:03:43 +0800

    feat: new helper for did:web generation

---
+++ crates/affinidi-messaging/affinidi-messaging-helpers/src/generate_did_web/main.rs
@@ -0,0 +1,192 @@
+//! Helps configure the various configuration options, DIDs and keys for the actors in the examples.
+//! This helps to create consistency in the examples and also to avoid code duplication.
+use affinidi_messaging_helpers::common::{affinidi_logo, check_path};
+use affinidi_tdk::dids::{DID, KeyType};
+use base64::prelude::*;
+use ring::signature::Ed25519KeyPair;
+use clap::Parser;
+use console::{Term, style};
+use did_peer::DIDPeerKeys;
+use serde_json::{Value, json};
+use std::{error::Error};
+
+/// Setups the environment for Affinidi Messaging
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the environments file (defaults to environments.json)
+    #[arg(long)]
+    host: Option<String>,
+
+    #[arg(long, short)]
+    secure_connection: Option<bool>,
+
+    #[arg(long, short)]
+    api_prefix: Option<String>
+}
+
+fn generate_secrets_and_did() -> Result<(String, Value), Box<dyn Error>> {
+    let (did, secrets) = DID::generate_did_peer(
+        vec![
+            (DIDPeerKeys::Verification, KeyType::P256),
+            (DIDPeerKeys::Verification, KeyType::Ed25519),
+            (DIDPeerKeys::Encryption, KeyType::Secp256k1),
+            (DIDPeerKeys::Encryption, KeyType::P256),
+        ],
+        None,
+    )
+    .unwrap();
+
+    let secrets_as_json = serde_json::to_value(secrets)?;
+
+    Ok((did, secrets_as_json))
+}
+
+fn generate_jwt_secret() -> String {
+    BASE64_URL_SAFE_NO_PAD
+        .encode(Ed25519KeyPair::generate_pkcs8(&ring::rand::SystemRandom::new()).unwrap())
+}
+
+fn build_did_doc(domain: &str, did: &str, mut secrets: Value, secure_connection: bool, api_path: String) -> Result<String, Box<dyn Error>> {
+    // handle secrets
+    if let Value::Array(ref mut items) = secrets {
+        for (i, item) in items.iter_mut().enumerate() {
+            if let Value::Object(map) = item {
+                // replace ids to be did keys
+                map.insert("id".to_string(), json!(format!("{}#key-{}", did, i + 1)));
+                // Add `controller`
+                map.insert("controller".to_string(), json!(did));
+                // Handle `privateKeyJwk`
+                if let Some(Value::Object(ref mut jwk)) = map.remove("privateKeyJwk") {
+                    jwk.remove("d"); // Remove private part
+                    map.insert("publicKeyJwk".to_string(), Value::Object(jwk.clone()));
+                }
+            }
+        }
+    }
+
+    let http = if secure_connection {
+        "https"
+    } else {
+        "http"
+    };
+
+    let ws = if secure_connection {
+        "wss"
+    } else {
+        "ws"
+    };
+
+    let result = serde_json::to_string_pretty(&json!({
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://w3id.org/security/suites/jws-2020/v1"
+        ],
+        "id": did,
+        "verificationMethod": secrets,
+        "authentication": [
+            format!("{}#key-1", did),
+            format!("{}#key-2", did),
+
+          ],
+          "assertionMethod": [
+            format!("{}#key-1", did),
+            format!("{}#key-2", did),
+
+          ],
+          "keyAgreement": [
+            format!("{}#key-3", did),
+            format!("{}#key-4", did),
+          ],
+        "service": [
+        {
+            "id": format!("{}#service", did),
+            "type": "DIDCommMessaging",
+            "serviceEndpoint": [
+                {
+                    "uri": format!("{}://{}{}", http, domain.replace("%3A", ":"), api_path),
+                    "routingKeys": [],
+                    "accept": [
+                        "didcomm/v2"
+                    ]
+                },
+                {
+                    "uri": format!("{}://{}{}/ws", ws, domain.replace("%3A", ":"), api_path),
+                    "routingKeys": [],
+                    "accept": [
+                        "didcomm/v2"
+                    ]
+                }
+            ]
+        },
+        {
+            "id": format!("{}#auth", did),
+            "type": "Authentication",
+            "serviceEndpoint": format!("{}://{}{}/authenticate", http, domain.replace("%3A", ":"), api_path),
+        },
+    ]
+    }))?;
+
+    Ok(result)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let term = Term::stdout();
+    let _ = term.clear_screen();
+    affinidi_logo::print_logo();
+    // Ensure we are somewhere we should be...
+    check_path()?;
+
+    let args: Args = Args::parse();
+    let host = args.host.unwrap_or("localhost%3A7037".to_string());
+    let secure_connection = args.secure_connection.unwrap_or(false);
+    let api_prefix = args.api_prefix.unwrap_or("".to_string());
+
+    println!(
+        "{}",
+        style("Generating new DID info...").yellow(),
+    );
+    let (old_did, secrets_json) = generate_secrets_and_did()?;
+    let mut well_known_did_part = if api_prefix.len() > 0 {
+        format!("{}{}", api_prefix, "/.well-known")
+    } else {
+        String::from("")
+    };
+    well_known_did_part = well_known_did_part.replace("/", ":");
+    let new_did = format!("did:web:{}{}", &host, &well_known_did_part);
+    let did_doc = build_did_doc(&host, &new_did, secrets_json.clone(), secure_connection, api_prefix)?;
+    let mut secrets_string = serde_json::to_string_pretty(&secrets_json)?;
+    let jwt_secret = generate_jwt_secret();
+    
+    secrets_string = secrets_string.replace(&old_did, &new_did);
+    
+    // print out all info to be use for sel-hosted mediator
+    println!(
+        "{} {}",
+        style("DID Value:").green(),
+        &new_did,
+    );
+    println!(
+        "{}\n{}",
+        style("DID Secrets:").green(),
+        &secrets_string,
+    );
+    println!(
+        "{}\n{}",
+        style("DID Document:").green(),
+        &did_doc,
+    );
+    println!(
+        "{}\n{}",
+        style("JWT Secret:").green(),
+        &jwt_secret,
+    );
+
+    println!(
+        "\n{}",
+        style("DID, DID Doc, and secrets completed. Use the values to setup your self-hosted mediator.").green(),
+    );
+
+    Ok(())
+}
