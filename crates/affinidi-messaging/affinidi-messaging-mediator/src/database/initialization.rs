178d730e0d66e729b1a54febcdd3e2df991046b1
Glenn Gore <glenn.g@affinidi.com>
2025-11-05 01:39:15 -0800

    Code changes only - no new versions (#149)
    * Updating mediator database version
    
    Signed-off-by: Glenn Gore <glenn.g@affinidi.com>
    
    * Added secret_generator example to easily create secret material
    
    Signed-off-by: Glenn Gore <glenn.g@affinidi.com>
    
    ---------
    
    Signed-off-by: Glenn Gore <glenn.g@affinidi.com>

--- crates/affinidi-messaging/affinidi-messaging-mediator/src/database/initialization.rs
+++ crates/affinidi-messaging/affinidi-messaging-mediator/src/database/initialization.rs
@@ -88,8 +88,8 @@ impl Database {
                     self.upgrade_0_10_0(&config.security.global_acl_default)
                         .await?;
                     info!("Database schema version updated to ({})", mediator_version);
-                } else if schema_version < Version::parse("0.11.1").unwrap() {
-                    self.upgrade_0_11_1().await?;
+                } else if schema_version < Version::parse("0.11.3").unwrap() {
+                    self.upgrade_0_11_3().await?;
                     info!("Database schema version updated to ({})", mediator_version);
                 } else {
                     error!(
