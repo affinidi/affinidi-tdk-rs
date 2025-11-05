1cb6b01e4ea52dc2a0b339a1d9cfd0622321e7e2
Glenn Gore <glenn.g@affinidi.com>
2025-11-05 16:51:58 +0800

    Updating mediator database version
    Signed-off-by: Glenn Gore <glenn.g@affinidi.com>

--- crates/affinidi-messaging/affinidi-messaging-mediator/src/database/upgrades/v0_11_0.rs
+++ crates/affinidi-messaging/affinidi-messaging-mediator/src/database/upgrades/v0_11_0.rs
@@ -7,7 +7,7 @@ use crate::database::Database;
 use affinidi_messaging_mediator_common::errors::MediatorError;
 
 impl Database {
-    pub(crate) async fn upgrade_0_11_1(&self) -> Result<(), MediatorError> {
-        self.upgrade_change_schema_version("0.11.1").await
+    pub(crate) async fn upgrade_0_11_3(&self) -> Result<(), MediatorError> {
+        self.upgrade_change_schema_version("0.11.3").await
     }
 }
