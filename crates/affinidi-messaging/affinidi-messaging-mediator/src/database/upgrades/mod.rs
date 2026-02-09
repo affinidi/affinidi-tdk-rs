7dfb9424b671ead7e1915114bff1253eb2d7369f
Robert Kwolek <robert.k@affinidi.com>
2026-02-09 10:39:57 +0100

    feat: schema migration added for the current migration version

--- crates/affinidi-messaging/affinidi-messaging-mediator/src/database/upgrades/mod.rs
+++ crates/affinidi-messaging/affinidi-messaging-mediator/src/database/upgrades/mod.rs
@@ -5,8 +5,8 @@
 use super::Database;
 use affinidi_messaging_mediator_common::errors::MediatorError;
 
+pub(crate) mod current_version;
 pub(crate) mod v0_10_0;
-pub(crate) mod v0_11_0;
 
 impl Database {
     pub(crate) async fn upgrade_change_schema_version(
